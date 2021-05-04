32b5fcc104ce3fb12b5820e2ee0b888a9c56c5fd
urawa72 <urawa72@gmail.com>
2021-05-04 13:45:48 +0900

    add frontend

--- src/main/java/com/example/accessingdatarest/HomeController.java
+++ src/main/java/com/example/accessingdatarest/HomeController.java
@@ -1,12 +1,12 @@
 package com.example.accessingdatarest;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @RequestMapping(value = "/")
+    @GetMapping("/")
     public String index() {
-        return "index";
+        return "forward:/index.html";
     }
 }
