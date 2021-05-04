52c5e64cfd77cf31ebfc77cee559826c2003e905
urawa72 <urawa72@gmail.com>
2021-05-04 13:51:33 +0900

    add pages

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <h1>Home</h1>
+      <div>
+        <Link to="/setting">Setting</Link>
+      </div>
+    </>
+  );
+};
+
+export default Home;
---
+++ frontend/src/pages/Setting.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Setting: React.FC = () => {
+  return (
+    <>
+      <h1>Setting</h1>
+      <div>
+        <Link to="/">Back to Home</Link>
+      </div>
+    </>
+  );
+};
+
+export default Setting;
