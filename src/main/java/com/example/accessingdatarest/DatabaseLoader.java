ef189a48e62d003f86b1d5f37cf99288775f26ef
urawa72 <urawa72@gmail.com>
2021-05-05 11:05:00 +0900

    add seed data loader

---
+++ src/main/java/com/example/accessingdatarest/DatabaseLoader.java
@@ -0,0 +1,24 @@
+package com.example.accessingdatarest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseLoader implements CommandLineRunner {
+
+	private final PersonRepository repository;
+
+    @Autowired
+	public DatabaseLoader(PersonRepository repository) {
+		this.repository = repository;
+	}
+
+	@Override
+	public void run(String... strings) throws Exception {
+		this.repository.save(new Person("山田", "花子"));
+		this.repository.save(new Person("鈴木", "一郎"));
+		this.repository.save(new Person("佐藤", "二郎"));
+		this.repository.save(new Person("田中", "三郎"));
+	}
+}
--- src/main/java/com/example/accessingdatarest/Person.java
+++ src/main/java/com/example/accessingdatarest/Person.java
@@ -15,6 +15,13 @@ public class Person {
     private String firstName;
     private String lastName;
 
+    private Person() {}
+
+    public Person(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
     public String getFirstName() {
         return firstName;
     }
